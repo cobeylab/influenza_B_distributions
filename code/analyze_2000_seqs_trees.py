3e77764ad1617057ef7020ce2f6134e4c3bd8c84
Marcos Costa Vieira <mvieira@midway2-login1.rcc.local>
2020-07-16 13:12:27 -0500

    Additional annotations to 2000s NZ-Aus flu B trees

--- code/analyze_2000_seqs_trees.py
+++ code/analyze_2000_seqs_trees.py
@@ -12,6 +12,9 @@ Determines proximity to B/Victoria and B/Yamagata to detect reassortants.
 import sys
 from dendropy import Tree
 import os
+import csv
+
+
 
 
 def determine_lineage_status(tree, Vic_ref, Yam_ref):
@@ -33,24 +36,47 @@ def determine_lineage_status(tree, Vic_ref, Yam_ref):
     
     for node in list(Vic_nodes):
         if node.is_leaf():
+            isolate_id = node.taxon.label.split('|')[0]
+            isolate_id = isolate_id.replace(' ','_')
             
-            lineage_status[node.taxon.label.split('|')[0]] = 'B/Victoria'
+            lineage_status[isolate_id] = 'B/Victoria'
     
     for node in list(Yam_nodes):
         if node.is_leaf():
-            lineage_status[node.taxon.label.split('|')[0]] = 'B/Yamagata'
+            isolate_id = node.taxon.label.split('|')[0]
+            isolate_id = isolate_id.replace(' ','_')
+
+            lineage_status[isolate_id] = 'B/Yamagata'
     
     return lineage_status
             
  
-def annotate_tree(tree, lineage_status, segment_name):
+def annotate_tree(tree, lineage_status, isolate_metadata, segment_name):
     for node in tree.leaf_nodes():
-        node.taxon.label
-        
-        if node.taxon.label.split('|')[0] in lineage_status.keys():
+        isolate_id = node.taxon.label.split('|')[0]
+        isolate_id = isolate_id.replace(' ','_')
+
+        if isolate_id in lineage_status.keys():
+            node_lineage = lineage_status[isolate_id]  
             
-            node_lineage = lineage_status[node.taxon.label.split('|')[0]]            
+            if isolate_id in isolate_metadata.keys():
+                if 'country' not in node.annotations.values_as_dict().keys():
+                    node_country = isolate_metadata[isolate_id]['country']
+                    node.annotations.add_new('country', node_country)
+                
+                if 'year' not in node.annotations.values_as_dict().keys():
+                    node_year = isolate_metadata[isolate_id]['year']
+                    node.annotations.add_new('year', node_year)
+                    
+                if 'isolate_name' not in node.annotations.values_as_dict().keys():
+                    isolate_name = isolate_metadata[isolate_id]['isolate_name']
+                    
+                    print isolate_name
+                
+                    node.annotations.add_new('isolate_name', isolate_name)                
+                    
             node.annotations.add_new(segment_name + '_lineage', node_lineage)
+            
     return tree
 
         
@@ -66,6 +92,20 @@ def main(argv):
     Vic_ref = 'B/Victoria/2/87'
     Yam_ref = 'B/Yamagata/16/88'
     
+    # Read isolate metadata into a dictionary
+    with open('../data/gisaid_metadata/gisaid_metadata.csv', 'rU') as f:
+        gisaid_metadata = csv.DictReader(f)
+        
+        isolate_metadata = {}
+        for record in gisaid_metadata:
+            isolate_id = record['isolate_id']
+            metadata = record
+            del metadata['isolate_id']
+            isolate_metadata[isolate_id] = metadata
+    
+    
+    
+    
     # HA lineage status (which lineage each strain is closest to in HA tree)
     HA_lineage_status = determine_lineage_status(HA_tree, Vic_ref, Yam_ref)
     
@@ -73,12 +113,12 @@ def main(argv):
     NA_lineage_status = determine_lineage_status(NA_tree, Vic_ref, Yam_ref)
     
     # Annotate both trees with both HA and NA lineage status
-    HA_annotated_tree = annotate_tree(HA_tree, HA_lineage_status, 'HA')
-    HA_annotated_tree = annotate_tree(HA_annotated_tree, NA_lineage_status, 'NA')
+    HA_annotated_tree = annotate_tree(HA_tree, HA_lineage_status, isolate_metadata, 'HA')
+    HA_annotated_tree = annotate_tree(HA_annotated_tree, NA_lineage_status, isolate_metadata, 'NA')
     
     
-    NA_annotated_tree = annotate_tree(NA_tree, HA_lineage_status, 'HA')
-    NA_annotated_tree = annotate_tree(NA_annotated_tree, NA_lineage_status, 'NA')
+    NA_annotated_tree = annotate_tree(NA_tree, HA_lineage_status, isolate_metadata, 'HA')
+    NA_annotated_tree = annotate_tree(NA_annotated_tree, NA_lineage_status, isolate_metadata, 'NA')
     
     HA_annotated_tree.write(path = parent_dir + '/annotated_HA_tree.nex', schema = 'nexus')
     NA_annotated_tree.write(path = parent_dir + '/annotated_NA_tree.nex', schema = 'nexus')
